eee605fb94d2164be6a64c0376543547f123cbb6
alessandro.zaccolo <alessandro.zaccolo@bts.tech>
2019-05-05 12:07:05 +0200

    Add mongouri

--- src/main/java/tech/bts/herokusample/api/SampleApi.java
+++ src/main/java/tech/bts/herokusample/api/SampleApi.java
@@ -1,18 +1,44 @@
 package tech.bts.herokusample.api;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.omg.CosNaming.NamingContextExtPackage.StringNameHelper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+
 
 @RestController
 @RequestMapping(path = "/api")
 public class SampleApi {
 
+    private final MongoCollection<Document> words;
+    
+    public SampleApi(@Value("${mongoUri}") String mongoUri){
+        //MongoClient mongoClient = MongoClients.create("mongodb+srv://alessandrozaccolo:Bts!2018@test-yzmvr.mongodb.net/test?retryWrites=true");
+        MongoClient mongoClient = MongoClients.create(mongoUri);
+        MongoDatabase database = mongoClient.getDatabase("test");
+        this.words = database.getCollection("words");
+
+    }
+
     //@RequestMapping(method = RequestMethod.GET, path = "/hello")
     @GetMapping("/hello")
     public String sayHello(){
         return "Hello from sample app";
+    }
+
+    @GetMapping("/insert")
+    public void insertWord(@RequestParam String word){
+
+        Document appDoc = new Document().append("word", word).append("date", new Date());
+
+        words.insertOne(appDoc);
+
     }
 }
